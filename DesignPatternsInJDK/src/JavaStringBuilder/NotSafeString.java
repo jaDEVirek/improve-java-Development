1297434129efcfc2b709024d3c088e8357f56ea7
Wiktor Religo <wiciorny@gmail.com>
2021-06-14 22:56:10 +0200

    adding some features, starting module for threads

--- DesignPatternsInJDK/src/JavaStringBuilder/NotSafeString.java
+++ DesignPatternsInJDK/src/JavaStringBuilder/NotSafeString.java
@@ -3,6 +3,7 @@ package JavaStringBuilder;
 public class NotSafeString {
     public static StringBuilder stringBuilder = new StringBuilder();
     public static volatile int x;
+
     public static void main(String[] args) {
 
         new Thread(new NotSafeString().runner("A")).start();
@@ -11,17 +12,12 @@ public class NotSafeString {
     }
 
     public Runnable runner(String name) {
-        Runnable rn = new Runnable() {
-            @Override public void run() {
-                for (int i = 0; i <= 100; i++) {
-                    ++x;
-                    stringBuilder.append(name)
-                            .append("-"+x+"-");
-                }
-
+        return () -> {
+            for (int i = 0; i <= 100; i++) {
+                ++x;
+                stringBuilder.append(name)
+                        .append("-" + x + "-");
             }
         };
-        rn.run();
-        return rn;
     }
 }
