ad77eb37b326b699f6566708b53034b0134d9b2d
Wiktor Religo <wiciorny@gmail.com>
2021-06-26 23:18:36 +0200

    implement redirection for  2 paths of localhost, basic example

--- Eventsourcing-microservice/src/main/java/com/jadevirek/eventsourcingmicroservice/common/RoutLocatorConfig.java
+++ Eventsourcing-microservice/src/main/java/com/jadevirek/eventsourcingmicroservice/common/RoutLocatorConfig.java
@@ -1,12 +1,14 @@
 package com.jadevirek.eventsourcingmicroservice.common;
 
-
 import org.springframework.cloud.gateway.route.RouteLocator;
-
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ *  Redirection config for cloud gatwey for localhost:9999
+ *
+ */
 @Configuration
 public class RoutLocatorConfig {
     @Bean
@@ -14,10 +16,11 @@ public class RoutLocatorConfig {
         return builder.routes()
                 .route(specPred -> specPred.path("/home")
                         .filters(gatewayFilterSpec -> gatewayFilterSpec.setPath("/guides"))
-                        .uri("http://spring.io/"))
-                .route("facebook", predicateSpec -> predicateSpec.path("/facebook/@**")
-                        .filters(gatewayFilterSpec -> gatewayFilterSpec.rewritePath("/facebook/@(?<handle>.*","/${}handle"))
-                        .uri("http://facebook.com/@")).build();
+                         .uri("http://spring.io/"))
+                .route("github", predicateSpec -> predicateSpec.path("/github/**")
+                        .filters(fs -> fs.rewritePath("/github/(?<handle>.*)","/${handle}"))
+                        .uri("http://github.com/@")).build();
     }
+
 
 }
